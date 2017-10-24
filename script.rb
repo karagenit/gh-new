7061715fa4c99caff89a15db2ad69d66d5d693ba
Caleb Smith <karagenit@outlook.com>
2017-10-24 18:19:44 -0400

    Add OptionParser

--- script.rb
+++ script.rb
@@ -1,35 +1,57 @@
 #!/usr/bin/env ruby
 
 require 'octokit'
+require 'optparse'
 
-name = ARGV.pop
+options = {}
 
-# TODO read args for create_repository options
+OptionParser.new do |parser|
+    parser.banner = "Usage: gh-new REPO [OPTIONS]"
 
-if name.nil?
-    puts "Usage: gh-new [REPO]"
+    parser.on('-u', '--username USERNAME', 'Github Username.') do |username| # TODO --uname, --login
+        options[:user] = username
+    end
+
+    parser.on('-p', '--password PASSWORD', 'Github Password.') do |password| # TODO --pword
+        options[:password] = password
+    end
+
+    parser.on_tail("-h", "--help", "--usage", "Show this usage message and quit.") do |help|
+        puts parser.help
+        exit
+    end
+end.parse!
+
+options[:repo] = ARGV.pop # TODO add to OptionParser somehow?
+if options[:repo].nil?
+    puts "Usage: gh-new REPO [OPTIONS]"
     exit
 end
 
-print "Create repository with name \"#{name}\"? (y/n): "
+print "Create repository with name \"#{options[:repo]}\"? (y/n): "
 conf = gets.chomp.downcase
 unless conf == "y" || conf == "yes"
     puts "Cancelled."
     exit
 end
 
-print "Username: "
-uname = gets.chomp
-print "Password: "
-pword = gets.chomp
+if options[:user].nil?
+    print "Username: "
+    options[:user] = gets.chomp
+end
+
+if options[:password].nil?
+    print "Password: "
+    options[:password] = gets.chomp
+end
 
 # TODO catch exception
 
 # TODO read git credentials
 
-client = Octokit::Client.new(:login => uname, :password => pword)
+client = Octokit::Client.new(:login => options[:user], :password => options[:password]) # TODO can we rename options[:user] to options[:login] and just pass the hash?
 
-client.create_repository(name)
+client.create_repository(options[:repo])
 
 puts "Done!"
 
